47d988d68f012bfe7047547d037dfc83df4ee6d9
skdbsqls <skdbsqls98@naver.com>
2023-06-28 22:02:32 +0900

    로그인 회원가입 기능 구현

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+import Router from "./shared/Router";
+import Login from "./components/Login";
+import Singup from "./components/Signup";
+
+function App() {
+  return (
+    <>
+      <Router></Router>
+      {/* <Login /> */}
+      {/* <Singup /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/redux/modules/loginModal.js
@@ -0,0 +1,35 @@
+const OPEN_LOGIN = "modal/OPEN_LOGIN";
+const CLOSE_LOGIN = "modal/CLOSE_LOGIN";
+
+export const openLogin = () => {
+  return {
+    type: OPEN_LOGIN,
+  };
+};
+
+export const closeLogin = () => {
+  return {
+    type: CLOSE_LOGIN,
+  };
+};
+
+const initialState = {
+  isOpen: false,
+};
+
+const loginModal = (state = initialState, action) => {
+  switch (action.type) {
+    case OPEN_LOGIN:
+      return {
+        isOpen: true,
+      };
+    case CLOSE_LOGIN:
+      return {
+        isOpen: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default loginModal;
