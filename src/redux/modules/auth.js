5c6bffb3d5f89b843c95c7695c31175324c613d4
skdbsqls <skdbsqls98@naver.com>
2023-06-29 20:37:07 +0900

    로그인 회원가입 기능 구현

--- src/redux/modules/auth.js
+++ src/redux/modules/auth.js
@@ -1,39 +1,57 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "../../firebase";
-
 const SIGNUP_SUCCESS = "auth/SIGNUP_SUCCESS";
 const SIGNUP_FAILURE = "auth/SIGNUP_FAILURE";
 const LOGIN_SUCCESS = "auth/LOGIN_SUCCESS";
 const LOGIN_FAILURE = "auth/LOGIN_FAILURE";
+const LOGOUT_SUCCESS = "auth/LOGOUT_SUCCESS";
+const UPDATE_DISPLAYNAME = "auth/UPDATE_DISPLAYNAME";
+const UPDATE_PROFILEPIC = "auth/UPDATE_PROFILEPIC";
 
-export const signUp = (email, password) => {
-  try {
-    const userCredential = createUserWithEmailAndPassword(auth, email, password);
-    return {
-      type: SIGNUP_SUCCESS,
-      payload: userCredential.user,
-    };
-  } catch (error) {
-    return {
-      type: SIGNUP_FAILURE,
-      payload: error.message,
-    };
-  }
+export const signUpSuccess = (user) => {
+  return {
+    type: SIGNUP_SUCCESS,
+    payload: user,
+  };
 };
 
-export const login = (email, password) => {
-  try {
-    const userCredential = signInWithEmailAndPassword(auth, email, password);
-    return {
-      type: LOGIN_SUCCESS,
-      payload: userCredential.user,
-    };
-  } catch (error) {
-    return {
-      type: LOGIN_FAILURE,
-      payload: error.message,
-    };
-  }
+export const signUpFailure = (error) => {
+  return {
+    type: SIGNUP_FAILURE,
+    payload: error,
+  };
+};
+
+export const loginSuccess = (user) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload: user,
+  };
+};
+
+export const loginFailure = (error) => {
+  return {
+    type: LOGIN_FAILURE,
+    payload: error,
+  };
+};
+
+export const logOutSuccess = () => {
+  return {
+    type: LOGOUT_SUCCESS,
+  };
+};
+
+export const updateDisplayName = (payload) => {
+  return {
+    type: UPDATE_DISPLAYNAME,
+    payload,
+  };
+};
+
+export const updateProfilePic = (payload) => {
+  return {
+    type: UPDATE_PROFILEPIC,
+    payload,
+  };
 };
 
 const initialState = {
@@ -44,6 +62,7 @@ const initialState = {
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case SIGNUP_SUCCESS:
+      return state;
     case LOGIN_SUCCESS:
       return {
         ...state,
@@ -51,12 +70,36 @@ const authReducer = (state = initialState, action) => {
         error: null,
       };
     case SIGNUP_FAILURE:
+      return {
+        ...state,
+        user: null,
+        error: action.payload,
+      };
     case LOGIN_FAILURE:
       return {
         ...state,
         user: null,
         error: action.payload,
       };
+
+    // case DELETE_USER:
+    //   return { state };
+    // case UPDATE_DISPLAYNAME:
+    //   return {
+    //     ...state,
+    //     user: {
+    //       ...state.user,
+    //       displayName: action.payload,
+    //     },
+    //   };
+    // case UPDATE_PROFILEPIC:
+    //   return {
+    //     ...state,
+    //     user: {
+    //       ...state.user,
+    //       photoURL: action.payload,
+    //     },
+    //   };
     default:
       return state;
   }
--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,15 +1,15 @@
-const initialState = {
-  user: null,
-  isLogin: false,
-};
+// const initialState = {
+//   user: null,
+//   isLogin: false,
+// };
 
-const users = (state = initialState, action) => {
-  switch (action.type) {
-    case "ADD_USER":
-      return { ...state, user: action.payload, isLogin: true };
-    default:
-      return state;
-  }
-};
+// const users = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "ADD_USER":
+//       return { ...state, user: action.payload, isLogin: true };
+//     default:
+//       return state;
+//   }
+// };
 
-export default users;
+// export default users;
